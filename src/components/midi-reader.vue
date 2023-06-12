ffef12eaef099b396bce4943d514a63a0f1cc4e5
ndk2020 <khoanguyen91@outlook.com>
2023-06-12 18:00:22 +0700

    feat(fe): Update notes data

--- src/components/midi-reader.vue
+++ src/components/midi-reader.vue
@@ -7,7 +7,8 @@
       </div>
     </a-button>
 
-    <a-textarea class="text-area" v-model:value="midi_info.str" placeholder="timespan between notes" auto-size />
+    <h3> </h3>
+    <a-textarea class="text-area" v-model:value="midi_info.output_str" placeholder="timespan between notes" auto-size />
     <a-textarea v-model:value="song_info" placeholder="song info" auto-size />
   </div>
 </template>
@@ -16,6 +17,7 @@
 import { invoke } from "@tauri-apps/api/tauri";
 import { open } from "@tauri-apps/api/dialog";
 import { Icon } from "@iconify/vue";
+import { Note, Track } from "@/types";
 
 //---------
 // @props
@@ -31,11 +33,30 @@ const props = withDefaults(defineProps<Props>(), {
 //---------
 // @events
 //---------
-const song_str = ref();
 const song_info = ref("");
+
+interface NotesData {
+  on_list: Note[]
+  off_list:Note[]
+  timespans: string[]
+  names: string[]
+  velocities: string[] 
+  raw_str_arr: string[]
+}
+
+const notes_data = reactive<NotesData>({
+  on_list: new Array(),
+  off_list: new Array(),
+  timespans: new Array(),
+  names: new Array(),
+  velocities: new Array(),
+  raw_str_arr: new Array()
+})
+
 const midi_info = reactive({
   name: "",
-  str: "",
+  raw_str: "",
+  output_str: "",
   num_of_notes: "",
   timespan_min: "",
   timespan_max: "",
@@ -43,26 +64,88 @@ const midi_info = reactive({
 
 const on_click = async () => {
   const f = await open();
-  console.log(f);
-  invoke("read_midi_mt3", { filePathStr: f }).then((res) => {
-    if (!res) return;
-    midi_info.name = typeof f == "string" ? f.replace(/^.*[\\\/]/, "") : "";
-    midi_info.str = res.toString();
-    let arr = midi_info.str.split(",").map((x) => parseFloat(x));
-    midi_info.num_of_notes = arr.length.toString();
-    midi_info.timespan_min = Math.min(...arr).toString();
-    midi_info.timespan_max = Math.max(...arr).toString();
-    song_info.value = `
-      name: ${midi_info.name}
-      num_of_notes: ${midi_info.num_of_notes}
-      timespan(min - max): ${midi_info.timespan_min} - ${midi_info.timespan_max}
-    `;
-  });
+  console.log("read midi file: " + f);
 
   invoke("read_midi", {filePathStr: f}).then((res) => {
-    console.log(res);
+    if (res) {
+      let track = res as Track;
+      let notes = track.notes;
+      get_notes_data(notes);
+
+      let arr = notes_data.timespans.map((x) => parseFloat(x));
+      midi_info.name = typeof f == "string" ? f.replace(/^.*[\\\/]/, "") : "";
+      midi_info.num_of_notes = notes_data.timespans.length.toString();
+      midi_info.timespan_min = Math.min(...arr).toString();
+      midi_info.timespan_max = Math.max(...arr).toString();
+      midi_info.raw_str = notes_data.raw_str_arr.join(",");
+      song_info.value = `
+        name: ${midi_info.name}
+        num_of_notes: ${midi_info.num_of_notes}
+        timespan(min - max) value: ${midi_info.timespan_min} - ${midi_info.timespan_max}
+      `;
+    }  
   });
 };
+
+const get_notes_data = (notes: Note[]) => {
+  let notes_on = notes.filter(x => x.kind == "On");
+  let notes_off = notes.filter(x => x.kind == "Off");
+  notes_data.on_list = notes_on;
+  notes_data.off_list = notes_off;
+
+  console.assert(
+    notes_on.length == notes_off.length,
+    "notes on and notes off array must have same length"
+  );
+  // timespans 
+  let timespans: string[] = new Array()
+   notes_on.forEach((note_on,id) => {
+    let timespan = 
+    note_on.delta_time_in_seconds + notes_off[id].delta_time_in_seconds;
+    timespans.push(timespan.toString());
+  });
+
+  timespans = timespans.filter(e => parseFloat(e) > 0.01)
+  notes_data.timespans = timespans;
+
+  // note_number
+  let notes_name: string[] = new Array()
+  notes_on.forEach(note_on => {
+https://www.inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
+    to_note_name(note_on.note_number);
+    notes_name.push(note_on.note_number.toString());
+  });
+  notes_data.names = notes_name;
+  
+  //velocity
+  let velocities: string[] = new Array()
+  notes_on.forEach(note_on => {
+    velocities.push(note_on.velocity.toString());
+  });
+  notes_data.velocities = velocities;
+
+  //
+  let raw_str: string[] = new Array();
+  timespans.forEach((t, i) => {
+    let id = i.toString();
+    let number = notes_name[i];
+    let velocity = velocities[i]; 
+    raw_str.push(`id:${id}-t:${t}-n:${number}-v:${velocity}`);
+  })
+  notes_data.raw_str_arr = raw_str;
+  //
+
+}
+
+const to_note_name = (num: number): string => {
+  switch(num) {
+    case 96:
+      return "C7";
+    default:
+      return "";
+  }
+}
+
 </script>
 
 <style lang="scss">
---
+++ src/types/track.ts
@@ -0,0 +1,31 @@
+export interface Track {
+  id: number,
+  /// Tempo Meta-Event
+  /// (tt tt tt) 3 bytes that specify the amt of ms per quarter note
+  /// eg: 0x07A120 = 500_000 ms
+  ///quarter note duration in milisecons
+  ///amount of time(ie, microseconds) per quarter note
+  tempo: number,
+  /// num of clock pulses per quarter note
+  /// ticks per beat
+  division: number,
+  seconds_per_tick: number,
+  name: string,
+  num_of_notes: number,
+  notes: Note[],
+  timespans_mt3: number[]
+}
+
+export interface Note {
+  id: number,
+  delta_time: number,
+  kind: string,
+  channel: string,
+  note_number: number,
+  seconds_per_tick: number,
+  velocity: number,
+  /// delta-time: The time difference in ticks between 
+  /// the previous MIDI track event and the current one
+  /// delta-time in seconds
+  delta_time_in_seconds: number,
+}
